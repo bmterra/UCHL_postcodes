3d6a8206bf9277baf2c2a1c8349d27c2dbae2bd2
Bruno Terra <bterra@scalefactory.com>
2018-03-06 02:57:57 +0000

    Moving code; new code for multiple post codes

---
+++ lib/coords.rb
@@ -0,0 +1,42 @@
+
+def CoordinatesFromPostalCode(postal_code)
+    api='AIzaSyCiTTnFGSuXdlTcACgx5eGm9VLroAqvHds'
+        #https://developers.google.com/maps/documentation/javascript/get-api-key#client-id
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?&sensor=false&key=#{api}&components=postal_code:#{postal_code}")
+        request = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request["accept"] = "application/json"
+        http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    response = http.request(request)
+    r = JSON.parse(response.body)
+         # {"results"=>[
+     #   {"address_components"=>[
+     #     {"long_name"=>"N1C 4BT", "short_name"=>"N1C 4BT", "types"=>["postal_code"]},
+     #     {"long_name"=>"Kings Cross", "short_name"=>"Kings Cross", "types"=>["neighborhood", "political"]},
+     #     {"long_name"=>"London", "short_name"=>"London", "types"=>["postal_town"]},
+     #     {"long_name"=>"Greater London", "short_name"=>"Greater London", "types"=>["administrative_area_level_2", "political"]},
+     #     {"long_name"=>"England", "short_name"=>"England", "types"=>["administrative_area_level_1", "political"]},
+     #     {"long_name"=>"United Kingdom", "short_name"=>"GB", "types"=>["country", "political"]}
+     #   ],
+     #   "formatted_address"=>"Kings Cross, London N1C 4BT, UK",
+     #   "geometry"=>{"bounds"=>{"northeast"=>{"lat"=>51.5379906, "lng"=>-0.1263251}, "southwest"=>{"lat"=>51.5365016, "lng"=>-0.127554}},
+     #   "location"=>{"lat"=>51.5374518, "lng"=>-0.1268112},
+     #   "location_type"=>"APPROXIMATE",
+     #   "viewport"=>{"northeast"=>{"lat"=>51.5385950802915, "lng"=>-0.125590569708498},
+     #   "southwest"=>{"lat"=>51.5358971197085, "lng"=>-0.128288530291502}}},
+     #   "place_id"=>"ChIJaSCqtxcbdkgRmBE1hoDrXTk",
+     #   "types"=>["postal_code"]}],
+     #   "status"=>"OK"}
+    unless r['status'] == "OK"
+        if r.has_key?("error_message")
+            puts "Error: #{r['error_message']}"
+            return false
+        end
+    end
+    puts r['results'][0]['formatted_address']
+    return {
+        'lat' => r['results'][0]['geometry']['location']['lat'],
+        'lon' => r['results'][0]['geometry']['location']['lng']
+    }
+end
---
+++ lib/search.rb
@@ -0,0 +1,47 @@
+require_relative 'lib/clinics'
+require_relative 'lib/aux'
+require_relative 'lib/poly'
+
+def CalculateRegion(point)
+    object = Clinics.new
+
+    for key in object.clinics.keys do
+      poly = object.clinics[key]['poly']
+      clinics = object.clinics[key]['clinics']
+      region = key
+      if IsPointInPolygon(point,poly)
+          cList = []
+          aux = Aux.new
+
+          clinics.each do |clinic|
+              xd = point['lat'] - clinic['lat']
+              yd = point['lon'] - clinic['lon']
+              dist = aux.distance(point,clinic)
+              cList.push({
+                  'name' => clinic['name'],
+                  'distance' => dist
+              })
+          end
+
+          return [
+            200,
+            {'Content-Type' => 'application/json'},
+            {
+              "response" => true,
+              "address"  => data['address'],
+              "region"   => key,
+              "clinics"  => aux.orderDistance(cList)
+            }.to_json
+          ]
+      end
+    end
+
+    # Else, return false
+    return [
+      200,
+      {'Content-Type' => 'application/json'},
+      [{
+        "response" => false,
+      }].to_json
+    ]
+end
