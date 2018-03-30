9e9cc1bc3537a1ce37070e157417c8fd992f6532
Bruno Terra <bterra@scalefactory.com>
2018-03-30 09:52:15 +0100

    Call address instead of components

--- lib/coords.rb
+++ lib/coords.rb
@@ -1,8 +1,9 @@
 
 def CoordinatesFromPostalCode(postal_code)
     api='AIzaSyBHmsSS0VoGB7VShQS_WGrG16anXzml4Q8'
-        #https://developers.google.com/maps/documentation/javascript/get-api-key#client-id
-    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?&sensor=false&key=#{api}&components=postal_code:#{postal_code}")
+    #https://developers.google.com/maps/documentation/javascript/get-api-key#client-id
+    #uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?&sensor=false&key=#{api}&components=postal_code:#{postal_code}")
+    uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?&sensor=false&key=#{api}&address='London #{postal_code}, UK'")
     request = Net::HTTP::Get.new(uri.request_uri)
     request["accept"] = "application/json"
     http = Net::HTTP.new(uri.host, uri.port)
@@ -19,6 +20,7 @@ def CoordinatesFromPostalCode(postal_code)
         end
     end
     return {
+        'query'   => postal_code,
         'address' => r['results'][0]['formatted_address'],
         'lat'     => r['results'][0]['geometry']['location']['lat'],
         'lon'     => r['results'][0]['geometry']['location']['lng']
